52ed0e2eb20243ef78594fdac809172405a06878
ziplox <100974818+sercanfurunci@users.noreply.github.com>
2022-12-30 09:40:19 +0300

    redux-thunk

--- src/Home.js
+++ src/Home.js
@@ -9,27 +9,36 @@ import { setTotal } from "./store/actions/actions";
 import { setProduct } from "./store/actions/actions";
 import axios from "axios";
 import ChangeLanguageButton from "./components/utils/changeLanguage";
+import { Pagination } from "antd";
 
 export default function Home() {
   const roots = useSelector((state) => state.roots);
   const baskets = useSelector((state) => state.baskets);
   const products = useSelector((state) => state.products);
-
   const productList = products.productList;
-  console.log(productList);
+
+  console.log(productList.length);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage] = useState(20);
+
+  const indexOfLastPost = currentPage * postPerPage;
+  const indexOfFirstPost = indexOfLastPost - postPerPage;
+  const currentPosts = productList.slice(indexOfFirstPost, indexOfLastPost);
+
+  const changePage = (e) => {
+    setCurrentPage(e);
+  };
+
   const basket = baskets.basket;
   const admin = roots.admin;
   const total = roots.total;
 
   const dispatch = useDispatch();
-  const getData = async () => {
-    const result = await axios("https://jsonplaceholder.typicode.com/photos");
-    dispatch(setProduct(result.data));
-  };
 
   useEffect(() => {
-    getData();
-  }, []);
+    dispatch(setProduct());
+  }, [dispatch]);
 
   const totaliHesapla = () => {
     const toplam = basket.reduce((acc, item) => {
@@ -57,11 +66,19 @@ export default function Home() {
       )}
       {admin && <AppModal />}
       <div className="container products">
-        {productList.slice(4980).map((product) => (
+        {currentPosts.map((product) => (
           <Product key={product.id} product={product} products={productList} />
         ))}
       </div>
       {total > 0 && <Basket />}
+      <Pagination
+        style={{ textAlign: "center", padding: "10px" }}
+        defaultCurrent={1}
+        pageSize={postPerPage}
+        showSizeChanger={false}
+        total={productList.length}
+        onChange={changePage}
+      />
     </React.Fragment>
   );
 }
